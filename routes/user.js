6119043fbbc0fe041d3320ab458f689d175d246f
nuzzesick <mati280341@gmail.com>
2021-04-30 06:39:12 -0300

    added new validations on routes

--- routes/user.js
+++ routes/user.js
@@ -8,7 +8,10 @@ router.put('/:id', editUser);
 router.post(
   '/',
   [
+    check('name', 'Name is required').not().isEmpty(),
     check('email', 'Email is not valid').isEmail(),
+    check('password', 'Password must contain more than 6 characters').isLength({ min: 6 }),
+    check('role', 'Invalid role').isIn(['ADMIN_ROLE', 'USER_ROLE']),
   ],
   createUser
 );
